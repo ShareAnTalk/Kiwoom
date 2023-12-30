781778320fa5b8fe8a5d6a20ffd56f5bc30317cc
SANGWOO PARK <cyberprophet@hanmail.net>
2023-12-31 01:51:14 +0900

    ready to try backtesting

---
+++ OpenAPI.TR.Entity/RealType/FuturesOptionPriorityQuote.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace ShareInvest.RealType;
+
+/// <summary>선물옵션우선호가</summary>
+public class FuturesOptionPriorityQuote
+{
+    /// <summary>0.10.현재가</summary>
+    [DataMember, JsonProperty("현재가"), StringLength(0x10)]
+    public string? CurrentPrice
+    {
+        get; set;
+    }
+    /// <summary>1.27.(최우선)매도호가</summary>
+    [DataMember, JsonProperty("(최우선)매도호가"), StringLength(0x10)]
+    public string? TopPriorityAskPrice
+    {
+        get; set;
+    }
+    /// <summary>2.28.(최우선)매수호가</summary>
+    [DataMember, JsonProperty("(최우선)매수호가"), StringLength(0x10)]
+    public string? TopPriorityBidPrice
+    {
+        get; set;
+    }
+}
