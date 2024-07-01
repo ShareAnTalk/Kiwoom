793ba7d93249e34b1264cb229486535bf7913c39
SANGWOO PARK <cyberprophet@hanmail.net>
2024-07-02 01:33:11 +0900

    save daily options chart

--- OpenAPI.TR.Entity/Multiples/opt50068.cs
+++ OpenAPI.TR.Entity/Multiples/opt50068.cs
@@ -1,5 +1,6 @@
 using Newtonsoft.Json;
 
+using System.ComponentModel.DataAnnotations;
 using System.Runtime.Serialization;
 
 namespace ShareInvest.OpenAPI.Entity;
@@ -8,43 +9,49 @@ namespace ShareInvest.OpenAPI.Entity;
 public class MultiOpt50068
 {
     /// <summary>현재가</summary>
-    [DataMember, JsonProperty("현재가")]
-    public string? PresentPrice
+    [DataMember, JsonProperty("현재가"), StringLength(8), Required]
+    public string? CurrentPrice
     {
         get; set;
     }
+
     /// <summary>누적거래량</summary>
-    [DataMember, JsonProperty("누적거래량")]
-    public string? AccumulatedTradingVolume
+    [DataMember, JsonProperty("누적거래량"), StringLength(8), Required]
+    public string? TradingVolume
     {
         get; set;
     }
+
     /// <summary>일자</summary>
-    [DataMember, JsonProperty("일자")]
+    [DataMember, JsonProperty("일자"), StringLength(8), Key]
     public string? Date
     {
         get; set;
     }
+
     /// <summary>시가</summary>
-    [DataMember, JsonProperty("시가")]
-    public string? StartPrice
+    [DataMember, JsonProperty("시가"), StringLength(8), Required]
+    public string? OpenPrice
     {
         get; set;
     }
+
     /// <summary>고가</summary>
-    [DataMember, JsonProperty("고가")]
+    [DataMember, JsonProperty("고가"), StringLength(8), Required]
     public string? HighPrice
     {
         get; set;
     }
+
     /// <summary>저가</summary>
-    [DataMember, JsonProperty("저가")]
+    [DataMember, JsonProperty("저가"), StringLength(8), Required]
     public string? LowPrice
     {
         get; set;
     }
+
     /// <summary>전일종가</summary>
-    [DataMember, JsonProperty("전일종가")]
+    [DataMember, JsonProperty("전일종가"), StringLength(8)]
     public string? ClosingPriceOfTheDayBefore
     {
         get; set;
