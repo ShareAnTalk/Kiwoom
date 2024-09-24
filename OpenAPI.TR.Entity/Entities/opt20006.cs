130fb784466557007ffefd2bef94d5e26cf2324f
sangwoo park <cyberprophet@hanmail.net>
2024-09-25 03:15:58 +0900

    collect index charts

--- OpenAPI.TR.Entity/Entities/opt20006.cs
+++ OpenAPI.TR.Entity/Entities/opt20006.cs
@@ -13,21 +13,25 @@ public class Opt20006 : TR
     {
         get; set;
     }
+
     /// <summary>업종일봉조회</summary>
     [DataMember, JsonProperty("업종일봉조회")]
     public MultiOpt20006[]? MultiResponse
     {
         get; set;
     }
+
+    public override string[] Id => ["업종코드", "기준일자"];
+
     /// <summary>
     /// 1.업종코드
     /// 2.기준일자
     /// </summary>
-    public override string[] Id => new[] { "업종코드", "기준일자" };
     public override string[]? Value
     {
         get; set;
     }
+
     public override string? RQName
     {
         set
@@ -36,18 +40,23 @@ public class Opt20006 : TR
         }
         get => "업종일봉조회요청";
     }
+
     public override string TrCode
     {
         get => nameof(Opt20006);
     }
+
     public override int PrevNext
     {
         get; set;
     }
+
     public override string ScreenNo
     {
         get => LookupScreenNo;
     }
-    public override string[] Single => new[] { "업종코드" };
-    public override string[] Multiple => new[] { "현재가", "거래량", "일자", "시가", "고가", "저가", "거래대금", "대업종구분", "소업종구분", "종목정보", "전일종가" };
+
+    public override string[] Single => ["업종코드"];
+
+    public override string[] Multiple => ["현재가", "거래량", "일자", "시가", "고가", "저가", "거래대금", "대업종구분", "소업종구분", "종목정보", "전일종가"];
 }
