afb6d5cd144efd1af6ae456d48b8cd3f560a109b
sangwoo park <cyberprophet@hanmail.net>
2024-09-29 18:39:02 +0900

    record trade execution data

--- OpenAPI.TR.Entity/Entities/opt10003.cs
+++ OpenAPI.TR.Entity/Entities/opt10003.cs
@@ -13,14 +13,17 @@ public class Opt10003 : TR
     {
         get; set;
     }
+
+    public override string[] Id => ["종목코드"];
+
     /// <summary>
     /// 1.종목코드
     /// </summary>
-    public override string[] Id => new[] { "종목코드" };
     public override string[]? Value
     {
         get; set;
     }
+
     public override string? RQName
     {
         set
@@ -29,18 +32,23 @@ public class Opt10003 : TR
         }
         get => "체결정보요청";
     }
+
     public override string TrCode
     {
         get => nameof(Opt10003);
     }
+
     public override int PrevNext
     {
         get; set;
     }
+
     public override string ScreenNo
     {
         get => LookupScreenNo;
     }
-    public override string[] Single => Array.Empty<string>();
-    public override string[] Multiple => new[] { "시간", "현재가", "전일대비", "대비율", "우선매도호가단위", "우선매수호가단위", "체결거래량", "sign", "누적거래량", "누적거래대금", "체결강도" };
+
+    public override string[] Single => [];
+
+    public override string[] Multiple => ["시간", "현재가", "전일대비", "대비율", "우선매도호가단위", "우선매수호가단위", "체결거래량", "sign", "누적거래량", "누적거래대금", "체결강도"];
 }
--- OpenAPI.TR.Entity/Multiples/opt10003.cs
+++ OpenAPI.TR.Entity/Multiples/opt10003.cs
@@ -1,5 +1,6 @@
 using Newtonsoft.Json;
 
+using System.ComponentModel.DataAnnotations;
 using System.Runtime.Serialization;
 
 namespace ShareInvest.OpenAPI.Entity;
@@ -8,68 +9,78 @@ namespace ShareInvest.OpenAPI.Entity;
 public class MultiOpt10003
 {
     /// <summary>시간</summary>
-    [DataMember, JsonProperty("시간")]
+    [DataMember, JsonProperty("시간"), StringLength(6), Required]
     public string? Time
     {
         get; set;
     }
+
     /// <summary>현재가</summary>
-    [DataMember, JsonProperty("현재가")]
-    public string? ThePresentPrice
+    [DataMember, JsonProperty("현재가"), StringLength(0x10), Required]
+    public string? CurrentPrice
     {
         get; set;
     }
+
     /// <summary>전일대비</summary>
-    [DataMember, JsonProperty("전일대비")]
-    public string? ComparedToThePreviousDay
+    [DataMember, JsonProperty("전일대비"), StringLength(0x10)]
+    public string? ComparedToPreviousDay
     {
         get; set;
     }
+
     /// <summary>대비율</summary>
-    [DataMember, JsonProperty("대비율")]
+    [DataMember, JsonProperty("대비율"), StringLength(0x10)]
     public string? ContrastRatio
     {
         get; set;
     }
+
     /// <summary>우선매도호가단위</summary>
-    [DataMember, JsonProperty("우선매도호가단위")]
+    [DataMember, JsonProperty("우선매도호가단위"), StringLength(0x10)]
     public string? PreferredSellingPriceUnit
     {
         get; set;
     }
+
     /// <summary>우선매수호가단위</summary>
-    [DataMember, JsonProperty("우선매수호가단위")]
+    [DataMember, JsonProperty("우선매수호가단위"), StringLength(0x10)]
     public string? PreferredPurchasePriceUnit
     {
         get; set;
     }
+
     /// <summary>체결거래량</summary>
-    [DataMember, JsonProperty("체결거래량")]
-    public string? ConcludedTransactionVolume
+    [DataMember, JsonProperty("체결거래량"), StringLength(0x10), Required]
+    public string? Volume
     {
         get; set;
     }
+
     /// <summary>sign</summary>
-    [DataMember, JsonProperty("sign")]
+    [DataMember, JsonProperty("sign"), StringLength(0x10)]
     public string? Sign
     {
         get; set;
     }
+
     /// <summary>누적거래량</summary>
-    [DataMember, JsonProperty("누적거래량")]
-    public string? AccumulatedTradingVolume
+    [DataMember, JsonProperty("누적거래량"), StringLength(0x10), Key]
+    public string? CumulativeVolume
     {
         get; set;
     }
+
     /// <summary>누적거래대금</summary>
-    [DataMember, JsonProperty("누적거래대금")]
-    public string? AccumulatedTransactionAmount
+    [DataMember, JsonProperty("누적거래대금"), StringLength(0x10)]
+    public string? CumulativeTransactionAmount
     {
         get; set;
     }
+
     /// <summary>체결강도</summary>
-    [DataMember, JsonProperty("체결강도")]
-    public string? FasteningStrength
+    [DataMember, JsonProperty("체결강도"), StringLength(0x10), Required]
+    public string? Pressure
     {
         get; set;
     }
