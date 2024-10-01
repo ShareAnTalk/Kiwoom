5e342c29ac80b742ad77807d74bdc9fddd9de818
sangwoo park <cyberprophet@hanmail.net>
2024-10-01 17:53:26 +0900

    change in real-time registration method for futures option

--- OpenAPI.TR.Entity/Entities/OPT50006.cs
+++ OpenAPI.TR.Entity/Entities/OPT50006.cs
@@ -13,20 +13,25 @@ public class OPT50006 : TR
     {
         get; set;
     }
+
     /// <summary>선옵체결추이</summary>
     [DataMember, JsonProperty("선옵체결추이")]
     public MultiOPT50006[]? MultiResponse
     {
         get; set;
+
     }
+
+    public override string[] Id => ["종목코드"];
+
     /// <summary>
     /// 1.종목코드
     /// </summary>
-    public override string[] Id => new[] { "종목코드" };
     public override string[]? Value
     {
         get; set;
     }
+
     public override string? RQName
     {
         set
@@ -35,18 +40,23 @@ public class OPT50006 : TR
         }
         get => "선옵체결추이요청";
     }
+
     public override string TrCode
     {
         get => nameof(OPT50006);
     }
+
     public override int PrevNext
     {
         get; set;
     }
+
     public override string ScreenNo
     {
         get => LookupScreenNo;
     }
-    public override string[] Single => new[] { "종목명" };
-    public override string[] Multiple => new[] { "체결시간", "현재가", "대비기호", "전일대비", "체결량", "누적거래량", "미결제약정", "미결제증감", "전일거래량대비", "전일거래량", "기준가" };
+
+    public override string[] Single => ["종목명"];
+
+    public override string[] Multiple => ["체결시간", "현재가", "대비기호", "전일대비", "체결량", "누적거래량", "미결제약정", "미결제증감", "전일거래량대비", "전일거래량", "기준가"];
 }
